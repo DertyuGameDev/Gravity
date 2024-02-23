b5b88ac9deded47e15dfd42944d29714db359f49
SamuGasto <samuel.55321@gmail.com>
2024-02-23 18:54:24 -0300

    Movement
    Update PlayerInput
    Update PlayerMovement
    Add Physic Material Player and Floor

--- Assets/Scripts/Entities/Player/PlayerInputs.cs
+++ Assets/Scripts/Entities/Player/PlayerInputs.cs
@@ -11,9 +11,23 @@ public class PlayerInputs : MonoBehaviour
     [Header("Flags")]
     public Vector2 movement;
     public Vector2 look;
+    public bool sprint;
+    public bool jump;
 
     void OnLook(InputValue value)
     {
         look = value.Get<Vector2>();
+    }
+    void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+    void OnSprint(InputValue value)
+    {
+        sprint = value.Get<float>() > 0? true : false;
+    }
+    void OnJump(InputValue value)
+    {
+        jump = value.Get<float>() > 0 ? true : false;
     }
 }
