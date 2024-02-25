dac2f4b825697592289409931dd3d7758bf76c47
Gab <160644362+rznn7@users.noreply.github.com>
2024-02-25 16:48:18 +0100

    [dev] dialogues add audio clip

--- Assets/Scripts/UI/Subtitles/Subtitle.cs
+++ Assets/Scripts/UI/Subtitles/Subtitle.cs
@@ -9,15 +9,17 @@ public class Subtitle
     public float duration;
     public Color color;
     public FontStyles fontStyle;
+    public AudioClip audioClip;
 
     public Subtitle()
     {
     }
 
-    public Subtitle(string text, float duration, Color color)
+    public Subtitle(string text, float duration, Color color, AudioClip audioClip)
     {
         this.text = text;
         this.duration = duration;
         this.color = color;
+        this.audioClip = audioClip;
     }
 }
