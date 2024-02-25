1fbe99a28bb12878580638d31ecd357550f46a8c
graphicmismatch <riptide.syncopate@gmail.com>
2024-02-25 20:01:32 +0530

    [dev]brought back old magnets

---
+++ Assets/Scripts/Entities/Objects/Puzzle/Magnets/MagnetsLegacy/Magnets/Pole.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pole : MonoBehaviour
+{
+    [Range(0,1)]
+    public int polarity;
+    public float strength;
+}
